da130092696834684231e7e2f48379efa19ba6a1
Benjamin Soltoff <ben@bensoltoff.com>
2018-06-13 11:02:41 -0500

    Convert tidytext code to a function

--- tidy.R
+++ tidy.R
@@ -13,33 +13,44 @@ episodes <- list.files(path = "season", full.names = TRUE)
 ## remove duplicate episodes
 episodes <- episodes[!str_detect(episodes, pattern = "outtakes|uncut")]
 
-# scrape the transcript of a single episode
-episode <- episodes[[1]]
+# function to scrape a transcript and convert to tidytext data frame
+tidy_episode <- function(url) {
+  ## collect text into a corpus
+  episode_corpus <- read_html(url) %>%
+    html_nodes("p") %>%
+    html_text(trim = TRUE)
+  
+  # convert to one-line-per-row data frame
+  episode_lines <- data_frame(line = episode_corpus) %>%
+    # detect scene transitions
+    mutate(scene = str_detect(line, "Scene"),
+           scene_num = cumsum(scene)) %>%
+    # remove scene transition lines
+    filter(scene_num != 0,
+           !scene) %>%
+    select(-scene) %>%
+    # determine which character is speaking
+    mutate(character = str_extract(line, "\\w+:"),
+           character = str_remove(character, ":"),
+           line = str_remove(line, "\\w+:")) %>%
+    # remove lines that are not speech
+    filter(!is.na(character))
+  
+  # convert to tidytext data frame
+  episode_tidy <- episode_lines %>%
+    unnest_tokens(output = word,
+                  input = line)
+  
+  return(episode_tidy)
+}
 
-## collect text into a corpus
-episode_corpus <- read_html(episode) %>%
-  html_nodes("p") %>%
-  html_text(trim = TRUE)
+# scrape all the transcripts
+episodes_tidy_all <- data_frame(episode = episodes) %>%
+  # safely tokenize each episode
+  mutate(tidy = map(episode, safely(tidy_episode)),
+         tidy_results = transpose(tidy)$result) %>%
+  # expand to one row per token
+  unnest(tidy_results) %>%
+  mutate(episode = parse_number(episode))
 
-## convert to one-line-per-row data frame
-episode_lines <- data_frame(episode = parse_number(episode),
-           line = episode_corpus) %>%
-  # detect scene transitions
-  mutate(scene = str_detect(line, "Scene"),
-         scene_num = cumsum(scene)) %>%
-  # remove scene transition lines
-  filter(scene_num != 0,
-         !scene) %>%
-  select(-scene) %>%
-  # determine which character is speaking
-  mutate(character = str_extract(line, "\\w+:"),
-         character = str_remove(character, ":"),
-         line = str_remove(line, "\\w+:")) %>%
-  # remove lines that are not speech
-  filter(!is.na(character))
-
-## convert to tidytext data frame
-episode_tidy <- episode_lines %>%
-  unnest_tokens(output = word,
-                input = line)
 
