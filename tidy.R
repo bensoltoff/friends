e8943f9ab8a9f269fd8c26453fc2b464297da0ec
Benjamin Soltoff <ben@bensoltoff.com>
2018-06-13 10:51:29 -0500

    Extract text corpus and convert to one-line-per-row data frame

--- tidy.R
+++ tidy.R
@@ -12,3 +12,27 @@ episodes <- list.files(path = "season", full.names = TRUE)
 
 ## remove duplicate episodes
 episodes <- episodes[!str_detect(episodes, pattern = "outtakes|uncut")]
+
+# scrape the transcript of a single episode
+episode <- episodes[[1]]
+
+## collect text into a corpus
+episode_corpus <- read_html(episode) %>%
+  html_nodes("p") %>%
+  html_text(trim = TRUE)
+
+## convert to one-line-per-row data frame
+episode_lines <- data_frame(episode = parse_number(episode),
+           line = episode_corpus) %>%
+  # detect scene transitions
+  mutate(scene = str_detect(line, "Scene"),
+         scene_num = cumsum(scene)) %>%
+  # remove scene transition lines
+  filter(scene_num != 0,
+         !scene) %>%
+  # determine which character is speaking
+  mutate(character = str_extract(line, "\\w+:"),
+         character = str_remove(character, ":")) %>%
+  # remove lines that are not speech
+  filter(!is.na(character))
+
